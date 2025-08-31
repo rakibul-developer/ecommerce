9e738151a77249d7f128b886763aba9f56153ce3
MD.Rakibul Islam <rakibprodan1395@gmail.com>
2025-08-31 20:30:09 +0600

    changes 8

--- user/views.py
+++ user/views.py
@@ -1,40 +1,42 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework import status
 from django.contrib.auth import authenticate
 
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Extra claims (optional)
+        token['username'] = user.username
+        return token
+
 class CustomTokenObtainPairView(TokenObtainPairView):
-    permission_classes = [AllowAny]  # Allow login without authentication
+    serializer_class = CustomTokenObtainPairSerializer
 
     def post(self, request, *args, **kwargs):
-        # Extract username and password from the request body
         username = request.data.get('username')
         password = request.data.get('password')
-        
-        # Authenticate the user manually using the credentials
+
         user = authenticate(username=username, password=password)
-        
-        # If authentication fails, raise an error
         if user is None:
-            raise AuthenticationFailed("Invalid username or password")
-        
-        # Set the user on the request
-        request.user = user
+            return Response(
+                {"detail": "Invalid username or password"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
 
-        # Call the default TokenObtainPairView logic to get the token
-        response = super().post(request, *args, **kwargs)
+        # Generate token
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        # Add user details to the response if the user is authenticated
-        if request.user.is_authenticated:
-            user_data = {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email if user.email else '',
-                'first_name': user.first_name if user.first_name else '',
-                'last_name': user.last_name if user.last_name else ''
-            }
-            # Add user details to the response
-            response.data.update(user_data)
-
-        return response
+        data = serializer.validated_data
+        # Add user details
+        data.update({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name
+        })
+        return Response(data, status=status.HTTP_200_OK)
