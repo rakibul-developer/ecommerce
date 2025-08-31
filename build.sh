46eb97e80a8bae0a1957d50eae652554819df443
MD.Rakibul Islam <rakibprodan1395@gmail.com>
2025-09-01 00:24:08 +0600

    revert some file

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements/requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+# if [[ $CREATE_SUPERUSER ]]
+# then
+#     python manage.py createsuperuser --no-input
+# fi
