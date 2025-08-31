834f80a3889a504986fc409429eca160c6c8f132
MD.Rakibul Islam <rakibprodan1395@gmail.com>
2025-08-31 20:18:06 +0600

    changes 7

--- frontend/src/views/LogIn.vue
+++ frontend/src/views/LogIn.vue
@@ -65,6 +65,8 @@ export default {
             await axios
                 .post('/api/v1/user/token/login/', formData)  // নতুন API URL
                 .then(response => {
+                    console.log('Login response:', response) // লগিন রেসপন্স চেক করার জন্য
+                    console.log(response.data)
                     const accessToken = response.data.access   // Access token
                     const refreshToken = response.data.refresh // Refresh token
 
